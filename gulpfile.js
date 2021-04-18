2206e5baca5d8212b8aa60f1f98b47235941e3a4
DmitryLancer20 <timofeev.dooma@yandex.ru>
2021-04-18 20:31:16 +0300

    added header

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,7 @@ let gulp = require('gulp'),
 	 cssmin = require('gulp-cssmin');
 
 	 gulp.task('sass', function(){
-			return gulp.src('app/scss/style.scss')
+			return gulp.src('app/scss/**/*.scss')
 					.pipe(sass({outputStyle: 'compressed'}))
 					.pipe(rename({suffix: '.min'}))
 					.pipe(autoprefixer({
@@ -20,6 +20,7 @@ let gulp = require('gulp'),
 
 		gulp.task('style', function(){
 			return gulp.src([
+				'node_modules/normalize.css/normalize.css',
 				'node_modules/slick-carousel/slick/slick.css',
 				'node_modules/magnific-popup/dist/magnific-popup.css'
 			])
@@ -57,7 +58,7 @@ let gulp = require('gulp'),
 		});
 
 gulp.task('watch', function(){
-	gulp.watch('app/scss/style.scss', gulp.parallel('sass'));
+	gulp.watch('app/scss/**/*.scss', gulp.parallel('sass'));
 	gulp.watch('app/*.html', gulp.parallel('html'));
 	gulp.watch('app/js/*.js', gulp.parallel('js'));
 });
