ae6b3285db2cbed6bf6a3534ce486389bbeb20db
DmitryLancer20 <timofeev.dooma@yandex.ru>
2021-04-24 16:46:52 +0300

    added libs.min.js page

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,17 @@
 $(function(){
 
+	$('.rate-star').rateYo({
+		rating: 5,
+		starWidth: "12px",
+		readOnly: true
+	 });
+
+	 $('.product-slider__inner').slick({
+		dots: true,
+	 });
 
 
-
+	var mixer = mixitup ('.products__inner-box');
 
 
 });
